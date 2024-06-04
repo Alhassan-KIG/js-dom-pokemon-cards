4b2374a97b394d508059bd0a73e6adb741973395
Alhassan Kassama <alhassankassama@hotmail.com>
2024-06-04 11:20:17 +0300

    Alhassan Pokemon cards

--- src/index.js
+++ src/index.js
@@ -1,6 +1,54 @@
+const pokemonList = document.querySelector("ul.cards");
 
-console.log(data);
+for (i = 0; i < data.length; i++) {
+  // creating the li element
+  const listItem = document.createElement("li");
+  listItem.classList = "card";
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+  // creating the h2
+  const heading = document.createElement("h2");
+  heading.classList = "card--title";
+  heading.textContent = data[i].name;
+
+  // adding the h2 as a child of li
+  listItem.appendChild(heading);
+
+  // creating the img
+  const image = document.createElement("img");
+  image.classList = "card--img";
+
+  image.setAttribute("width", "256");
+
+  image.setAttribute(
+    "src",
+    data[i].sprites.other["official-artwork"].front_default
+  );
+
+  // adding the img as a child of the li
+  listItem.appendChild(image);
+
+  // creating the list for pokemon stats
+  const statsList = document.createElement("ul");
+  statsList.classList = "card--text";
+
+  // loop over pokemon stats
+  for (j = 0; j < data[i].stats.length; j++) {
+    // create li for individual stat
+    const statListItem = document.createElement("li");
+    // create text for stat
+    const text =
+      data[i].stats[j].stat.name.toUpperCase() +
+      ": " +
+      data[i].stats[j].base_stat;
+    statListItem.textContent = text;
+
+    // adding li ul
+    statsList.appendChild(statListItem);
+  }
+
+  // adding ul to li
+  listItem.appendChild(statsList);
+
+  // adding the li as a child of the ul
+  pokemonList.appendChild(listItem);
+}
